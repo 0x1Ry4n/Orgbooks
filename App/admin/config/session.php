5281bebabde1824a8e29a984dfe0cc09bb86b25d
0x1Ry4n <ryanlucas2018gz@gmail.com>
2022-11-09 18:43:58 -0300

    remove App

--- App/admin/config/session.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start(); 
-
-    if (!isset($_SESSION['authlogin']) || (trim($_SESSION['authlogin']) == '')) {
-        echo "<script> window.location = '../index.php'; </script>";
-    }
-    
-    $session_id = $_SESSION['authlogin'];
-?>
